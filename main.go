b9f34f66ce92d2401e5a3c1c0e9ac69cb6dc36c2
Keshav Chand <chandkeshav001@gmail.com>
2022-03-29 22:57:11 +0530

    Better conditional for inserting

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
+import "math"
+
 type OrderBook struct {
 	BuyOrders  *OrderLevel
-	BestBuy    float32 // Highest
 	SellOrders *OrderLevel
-	BestSell   float32 // Lowest
 }
 
 type OrderLevel struct {
@@ -14,7 +14,7 @@ type OrderLevel struct {
 	LesserLevel  *OrderLevel
 }
 
-func (currentLevel *OrderLevel) Insert(order Order) {
+func (currentLevel *OrderLevel) Insert(order Order) *OrderLevel {
 	// If orders are present then iterate
 	// to the level least less than the required level
 	level := currentLevel
@@ -22,43 +22,44 @@ func (currentLevel *OrderLevel) Insert(order Order) {
 		for level.GreaterLevel != nil {
 			if level.Price == order.Price {
 				level.CurrentOrder = append(level.CurrentOrder, order)
-				return
+				return nil
 			}
 			if level.GreaterLevel.Price > order.Price {
 				break
 			}
 			level = level.GreaterLevel
 		}
-		newLevel := OrderLevel{
+		newLevel := &OrderLevel{
 			Price:        order.Price,
-			CurrentOrder: make([]Order, 0, 100),
+			CurrentOrder: make([]Order, 0, 10),
 			GreaterLevel: level.GreaterLevel,
 			LesserLevel:  level,
 		}
-		level.GreaterLevel = &newLevel
-		return
+		level.GreaterLevel = newLevel
+		return newLevel
 	}
 	if level.Price > order.Price {
 		for level.LesserLevel != nil {
 			if level.Price == order.Price {
 				level.CurrentOrder = append(level.CurrentOrder, order)
-				return
+				return nil
 			}
 			if level.LesserLevel.Price < order.Price {
 				break
 			}
-			level = level.GreaterLevel
+			level = level.LesserLevel
 		}
-		newLevel := OrderLevel{
+		newLevel := &OrderLevel{
 			Price:        order.Price,
 			CurrentOrder: make([]Order, 0, 100),
 			GreaterLevel: level,
 			LesserLevel:  level.LesserLevel,
 		}
-		level.LesserLevel = &newLevel
-		return
+		level.LesserLevel = newLevel
+		return newLevel
 	}
 
+	return nil // HOW IT REACHED HERE
 }
 
 type OrderSide int
@@ -82,6 +83,19 @@ type Order struct {
 	Size  int
 }
 
+func (book *OrderBook) BestBuy() float32 {
+	if book.BuyOrders == nil {
+		return math.SmallestNonzeroFloat32
+	}
+	return book.BuyOrders.Price
+}
+func (book *OrderBook) BestSell() float32 {
+	if book.SellOrders == nil {
+		return math.MaxFloat32
+	}
+	return book.SellOrders.Price
+}
+
 func (book *OrderBook) Insert(order Order) {
 	switch order.Side {
 	case BUY:
@@ -92,20 +106,22 @@ func (book *OrderBook) Insert(order Order) {
 			if book.BuyOrders == nil {
 				level := OrderLevel{
 					Price:        order.Price,
-					CurrentOrder: make([]Order, 0, 100),
+					CurrentOrder: make([]Order, 0, 10),
 				}
 				level.CurrentOrder = append(level.CurrentOrder, order)
 				book.BuyOrders = &level
-				book.BestBuy = order.Price
 				return
 			}
 		}
-    if order.Price < book.BestSell {
-      book.BuyOrders.Insert(order)
-      if order.Price > book.BestBuy {
-        book.BestBuy = order.Price
-      }
-    }
+		// NO one is selling lower than the least
+		// selling price
+		if order.Price < book.BestSell() {
+			newOrder := book.BuyOrders.Insert(order)
+			if newOrder != nil {
+				book.BuyOrders = newOrder
+			}
+			return
+		}
 	case SELL:
 		if book.BuyOrders == nil {
 			if order.Type == MARKET {
@@ -114,20 +130,22 @@ func (book *OrderBook) Insert(order Order) {
 			if book.SellOrders == nil {
 				level := OrderLevel{
 					Price:        order.Price,
-					CurrentOrder: make([]Order, 0, 100),
+					CurrentOrder: make([]Order, 0, 10),
 				}
 				level.CurrentOrder = append(level.CurrentOrder, order)
 				book.SellOrders = &level
-				book.BestBuy = order.Price
 				return
 			}
 		}
-    if order.Price > book.BestBuy {
-      book.SellOrders.Insert(order)
-      if order.Price < book.BestBuy {
-        book.BestBuy = order.Price
-      }
-    }
+		// NO one is buying at higher price
+		// than the highest
+		if order.Price > book.BestBuy() {
+			newOrder := book.SellOrders.Insert(order)
+			if newOrder != nil {
+				book.SellOrders = newOrder
+			}
+			return
+		}
 	}
 }
 
